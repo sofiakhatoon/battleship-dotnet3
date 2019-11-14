341e739871bd62d0ba17c9969a88db928623e2bb
sofiakhatoon <44379240+sofiakhatoon@users.noreply.github.com>
2019-11-14 10:10:51 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BattleshipModellingPractice.Objects;
+using BattleshipModellingPractice.Objects.Games;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipModellingPractice_master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many games you want to play?");
+            var numGames =1;// int.Parse(Console.ReadLine());
+            Game game = new Game();
+            game.PlayToEnd();            
+        }
+    }
+}
